9ebf77c251b55ed7ef379825c283d49a0766e57e
George Grigorev <deezchannel@gmail.com>
2017-01-31 08:37:07 +0300

    I screwed up a little bit while trying to get my git commits right :\

--- src/Hello.hs
+++ src/Hello.hs
@@ -1,2 +1,1 @@
-module Hello where
-
+main = putStrLn "Hello, world!"
--- src/Sandbox.hs
+++ src/Sandbox.hs
@@ -1,2 +1,16 @@
 module Sandbox where
 
+-- some functions demonstrating Haskell syntax stuff
+sumSquares x y = x ^ 2 + y ^ 2
+sign x = if x > 0 then 1 else if x < 0 then -1 else 0
+max5 x = max 5 x
+max5' = max 5
+
+
+-- my own operators
+infixl 6 *+*
+
+a *+* b = sumSquares a b
+a |-| b = abs (a - b)
+
+
